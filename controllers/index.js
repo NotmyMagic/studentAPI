f2ea4f9833d193a74939f7d61ccafda609b8d8c1
Cale Sumner <calecsumner@gmail.com>
2024-11-26 10:35:18 -0700

    added student api

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,6 @@
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
+
 const awesomeFunction =
   ("/",
   (req, res) => {
@@ -10,5 +13,16 @@ const ttech =
     res.send("TooeleTech is Awesome!");
   });
 
-module.exports = { awesomeFunction, ttech };
-// module.exports = { ttech }; this doesen't work they have to be the same
+const getAllStudents = async (req, res) => {
+  try {
+    const result = await mongodb.getDb().db().collection("students").find();
+    result.toArray().then((lists) => {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists);
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+module.exports = { awesomeFunction, ttech, getAllStudents };
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const routes = require("express").Router();
 
 routes.get("/", myController.awesomeFunction);
 routes.get("/ttech", myController.ttech);
+routes.get("/students", myController.getAllStudents);
 
 module.exports = routes;
